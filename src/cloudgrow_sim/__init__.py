fd36d21863b40ff7b6bc8894d1522163619ad902
Robert Allen <bob@epicpastures.com>
2025-12-13 23:55:50 -0500

    chore: bump version to 0.1.1

--- src/cloudgrow_sim/__init__.py
+++ src/cloudgrow_sim/__init__.py
@@ -1,4 +1,4 @@
 """A simulator for greenhouse environments to facilitate training and estimations of tooling and servicing."""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["__version__"]
